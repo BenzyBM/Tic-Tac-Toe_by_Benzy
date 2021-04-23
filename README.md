9ad574147e508a3b4b21b5bcb39632822707fd47
Benzy <vipbence@gmail.com>
2021-04-24 00:23:30 +0200

    Version b_1.1
    README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Tic-Tac-Toe_by_Benzy
+# Tic-Tac-Toe von Benzy
  Python Tic-Tac-Toe for learning Python and for a project
+
+ Willkommen in meinem Tic-Tac-Toe Spiel [das nie endet]
+ 
+ Die Regeln sind einfach:  -Wähle eine Zahl zwischen 1 und 9 
+                           -Wenn die Zahl größer oder gleich groß ist, wie die, 
+                            von dem Computer, dann fangen Sie an. Ansonsten der PC
+                           -Die Kacheln sind durchnummeriert (1- 2^n) und man wählt
+                            eine beliebige Zahl aus, die dann entsprechend durch das 
+                            eigene Zeichen belegt wird
+                           -Sobald man n konsequente Zeichen hat, gewinnt man... oder so...
+                           -Wenn man sich am Anfang des Spieles verschreibt,
+                            dann fängt der Computer automatisch an :D
+
+ Viel Spaß und vor allem, viel Erfolg
+
--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,26 +1,3 @@
-# Willkommen in meinem Tic-Tac-Toe Spiel [das nie endet]
-# 
-# Die Regeln sind einfach:  -Wähle eine Zahl zwischen 1 und 9 
-#                           -Wenn die Zahl größer oder gleich groß ist, wie die, 
-#                            von dem Computer, dann fangen Sie an. Ansonsten der PC
-#                           -Die Kacheln sind durchnummeriert (1- 2^n) und man wählt
-#                            eine beliebige Zahl aus, die dann entsprechend durch das 
-#                            eigene Zeichen belegt wird
-#                           -Sobald man n konsequente Zeichen hat, gewinnt man... oder so...
-#                           -Wenn man sich am Anfang des Spieles verschreibt,
-#                            dann fängt der Computer automatisch an :D
-#
-# Viel Spaß und vor allem, viel Erfolg
-
-# Funktionen im Hauptmodul
-#  - Ermittlung des Spielstartes
-#  - Erste Schrite
-#  - Rundenwechsel
-#  - Stand nach jeder Runde anzeigen
-#  - Erweiterung  
-#
-#  - Neue Runde, wenn man abbrechen will
-
 import random as rand
 
 # globale Variablen
